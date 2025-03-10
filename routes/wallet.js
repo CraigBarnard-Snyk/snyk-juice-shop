d20748a570c35a07f13c66c9446e948e75573f6b
Craig Barnard <craig.barnard@snyk.io>
2025-03-11 10:08:33 +1300

    feat: add a vulnerability

--- routes/wallet.js
+++ routes/wallet.js
@@ -7,6 +7,7 @@ const models = require('../models/index')
 
 module.exports.getWalletBalance = function getWalletBalance () {
   return async (req, res, next) => {
+    password = '1234567890'
     const wallet = await models.Wallet.findOne({ where: { UserId: req.body.UserId } })
     if (wallet) {
       res.status(200).json({ status: 'success', data: wallet.balance })
